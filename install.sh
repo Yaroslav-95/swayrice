34f1124b141b66bc12cd9e0aa8d2b04a156164fe
Yaroslav <contact@yaroslavps.com>
2019-09-30 05:09:47 +0300

    plymouth, lightdm

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 # Install needed programs
 
-pacman -Sy --noconfirm networkmanager network-manager-applet networkmanager-openvpn neovim sway swaylock swayidle waybar egl-wayland imv swaybg xorg-server-xwayland qt5-wayland wl-clipboard dbus tmux htop bemenu mupdf mpd ncmpcpp mpc mpv mplayer mako cronie grim slop ffmpeg w3m youtube-dl youtube-viewer newsboat wget curl gimp inkscape gcc make fontconfig pkg-config fakeroot lxappearance papirus-icon-theme alsa-utils pulseaudio imagemagick ntfs-3g unzip unrar gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 perl-term-readline-gnu transmission-cli python-pynvim python-pip calcurse acpi acpid gnupg noto-fonts-cjk powerline-fonts python-pygit2 polkit polkit-gnome zathura zathura-pdf-mupdf zathura-djvu gst-libav vifm blueman bluez acpilight powertop
+pacman -Sy --noconfirm networkmanager network-manager-applet networkmanager-openvpn neovim sway swaylock swayidle waybar egl-wayland imv swaybg xorg-server-xwayland qt5-wayland wl-clipboard dbus tmux htop bemenu mupdf mpd ncmpcpp mpc mpv mplayer mako cronie grim slop ffmpeg w3m youtube-dl youtube-viewer newsboat wget curl gimp inkscape gcc make fontconfig pkg-config fakeroot lxappearance papirus-icon-theme alsa-utils pulseaudio imagemagick ntfs-3g unzip unrar gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 perl-term-readline-gnu transmission-cli python-pynvim python-pip calcurse acpi acpid gnupg noto-fonts-cjk powerline-fonts python-pygit2 polkit polkit-gnome zathura zathura-pdf-mupdf zathura-djvu gst-libav vifm blueman bluez acpilight powertop lightdm
 
 # Copy system config files
 cp system/sudoers /etc/
+cp system/mkinitcpio.conf /etc/
+cp -r system/loader/ /boot/loader/
+cp -r system/lightdm/ /etc/lightdm/
 cp systemd/hotspot.service /etc/systemd/system/
 cp systemd/startupsound.service /etc/systemd/system/
 
@@ -15,10 +18,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S --noconfirm ttf-symbola stig pam-gnupg mutt-wizard-git ncmpamixer
+yay -S --noconfirm ttf-symbola stig pam-gnupg mutt-wizard-git ncmpamixer lightdm-mini-greeter plymouth-git
 
 # Laptop
 pacman -S --noconfirm xf86-video-intel
 
+systemctl enable lightdm
 systemctl enable NetworkManager
 systemctl start NetworkManager
