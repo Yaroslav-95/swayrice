0ced8bb69521aeb9e6e103268b517711a1206108
Yaroslav de la Peña Smirnov <yps@yaroslavps.com>
2021-02-01 07:06:26 +0300

    Now using swaylock fork swaylock-effects

--- artix.sh
+++ artix.sh
@@ -18,9 +18,9 @@ pacman -Sy --noconfirm zsh zsh-syntax-highlighting openresolv wireguard-tools \
 	noto-fonts-cjk noto-fonts-emoji ttf-joypixels otf-latin-modern \
 	otf-latinmodern-math powerline-fonts python-pygit2 polkit polkit-gnome \
 	zathura zathura-pdf-mupdf zathura-djvu gst-libav vifm blueman bluez \
-	bluez-runit acpilight accountsservice lightdm xdg-user-dirs abook \
-	terminus-font qutebrowser jq stow wdiff texlive-most units isync notmuch \
-	pass translate-shell gnome-keyring pacman-contrib linux-headers \
+	bluez-runit acpilight accountsservice xdg-user-dirs abook terminus-font \
+	qutebrowser jq stow wdiff texlive-most units isync notmuch pass \
+	translate-shell gnome-keyring pacman-contrib linux-headers \
 	v4l2loopback-dkms highlight mediainfo
 
 # Copy system config files
@@ -40,7 +40,7 @@ cd build
 sudo -u "$1" "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
 
 yay -S --noconfirm aerc-git ttf-symbola ttf-ms-fonts pam-gnupg ncpamixer mimeo \
-	xdg-utils-mimeo cli-visualizer lf dragon-drag-and-drop
+	xdg-utils-mimeo cli-visualizer lf dragon-drag-and-drop swaylock-effects
 
 # Copy some fonts to make them available system-wide
 mkdir -p /usr/share/fonts/rice
