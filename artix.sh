a36aadab60c09bff3a055b0442971aa5e8a87a27
Yaroslav de la Peña Smirnov <yps@yaroslavps.com>
2022-05-18 01:17:06 +0300

    WIP: rofi
    * Moved basically all menu scripts to use rofi
    * Updated some custom desktop entries
    * Updated list of packages. TODO: make a better installer script/package
      list.
    * Removed some old and extraneous scripts.
    * Other minor changes to config.

--- artix.sh
+++ artix.sh
@@ -3,25 +3,129 @@
 # Very basic helper script to install some programs and configs for an
 # Artix+runit+Sway system
 
-pacman -Sy zsh zsh-syntax-highlighting wireguard-tools mediainfo highlight \
-	bind-tools sway swaylock swayidle waybar egl-wayland imv swaybg man-db \
-	xorg-server-xwayland alacritty qt5-wayland wl-clipboard dbus tmux htop \
-	bemenu bemenu-wlroots mupdf mpd ncmpcpp mpc mpv libnotify cronie \
-	cronie-runit metalog metalog-runit dnsmasq networkmanager dhcpcd \
-	networkmanager-runit rsync grim wf-recorder slurp ffmpeg w3m youtube-dl \
-	newsboat wget curl inkscape gimp darktable gcc make wpa_supplicant \
-	fontconfig pkg-config fakeroot papirus-icon-theme alsa-utils pipewire \
-	pipewire-alsa pipewire-pulse wireplumber pamixer imagemagick ntfs-3g unzip \
-	unrar gnome-themes-standard telegram-desktop qt5ct r bc udisks2 dash \
-	perl-term-readline-gnu transmission-cli transmission-remote-gtk \
-	python-pynvim python-pip python-pillow calcurse acpi acpid gnupg \
-	noto-fonts-cjk noto-fonts-emoji ttf-joypixels otf-latin-modern \
-	otf-latinmodern-math powerline-fonts python-pygit2 polkit polkit-gnome \
-	zathura zathura-pdf-mupdf zathura-djvu gst-libav blueman bluez \
-	bluez-runit acpilight accountsservice xdg-user-dirs terminus-font \
-	vimb jq stow wdiff texlive-most texlive-lang biber units isync neomutt \
-	notmuch pass translate-shell gnome-keyring pacman-contrib linux-headers \
-	openntpd openntpd-runit v4l2loopback-dkms || exit 1
+pacman -Sy \
+	zsh \
+	zsh-syntax-highlighting \
+	wireguard-tools \
+	mediainfo \
+	highlight \
+	bind-tools \
+	sway \
+	swaylock \
+	swayidle \
+	waybar \
+	egl-wayland \
+	imv \
+	swaybg \
+	man-db \
+	xorg-server-xwayland \
+	qt5-wayland \
+	wl-clipboard \
+	dbus \
+	tmux \
+	htop \
+	mupdf \
+	mpd \
+	ncmpcpp \
+	mpc \
+	mpv \
+	libnotify \
+	cronie \
+	cronie-runit \
+	metalog \
+	metalog-runit \
+	dnsmasq \
+	networkmanager \
+	dhcpcd \
+	networkmanager-runit \
+	rsync \
+	grim \
+	wf-recorder \
+	slurp \
+	ffmpeg \
+	w3m \
+	youtube-dl \
+	newsboat \
+	wget \
+	curl \
+	inkscape \
+	gimp \
+	darktable \
+	gcc \
+	clang \
+	make \
+	wpa_supplicant \
+	fontconfig \
+	pkg-config \
+	fakeroot \
+	papirus-icon-theme \
+	alsa-utils \
+	pipewire \
+	pipewire-alsa \
+	pipewire-pulse \
+	wireplumber \
+	pamixer \
+	imagemagick \
+	ntfs-3g \
+	unzip \
+	unrar \
+	gnome-themes-standard \
+	telegram-desktop \
+	qt5ct \
+	r \
+	bc \
+	udisks2 \
+	dash \
+	perl-term-readline-gnu \
+	transmission-cli \
+	transmission-remote-gtk \
+	python-pynvim \
+	python-pip \
+	python-pillow \
+	calcurse \
+	acpi \
+	acpid \
+	gnupg \
+	noto-fonts-cjk \
+	noto-fonts-emoji \
+	ttf-joypixels \
+	otf-latin-modern \
+	otf-latinmodern-math \
+	powerline-fonts \
+	python-pygit2 \
+	polkit \
+	polkit-gnome \
+	zathura \
+	zathura-pdf-mupdf \
+	zathura-djvu \
+	gst-libav \
+	blueman \
+	bluez \
+	bluez-runit \
+	acpilight \
+	accountsservice \
+	xdg-user-dirs \
+	terminus-font \
+	vimb \
+	jq \
+	stow \
+	wdiff \
+	texlive-most \
+	texlive-lang \
+	biber \
+	units \
+	isync \
+	neomutt \
+	notmuch \
+	pass \
+	translate-shell \
+	gnome-keyring \
+	pacman-contrib \
+	linux-headers \
+	openntpd \
+	openntpd-runit \
+	v4l2loopback-dkms \
+	|| exit 1
 
 # Copy system config files
 cp system/sudoers /etc/
@@ -53,7 +157,20 @@ chown -R "$1:$1" build
 cd build
 sudo -u "$1" "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
 
-yay -S --noconfirm ttf-symbola ttf-ms-fonts pam-gnupg ncpamixer mimeo \
-	xdg-utils-mimeo cli-visualizer lf dragon-drag-and-drop swaylock-effects \
-	youtube-viewer abook 
+yay -S --noconfirm \
+	ttf-symbola \
+	ttf-ms-fonts \
+	pam-gnupg \
+	ncpamixer \
+	mimeo \
+	xdg-utils-mimeo \
+	lf \
+	dragon-drag-and-drop \
+	swaylock-effects \
+	youtube-viewer \
+	abook \
+	compiledb \
+	rofi-lbonn-wayland \
+	rofi-calc \
+	rofi-file-browser-extended-git
 
